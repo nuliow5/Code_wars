3f075b41b4a85a1b654b04d469970c25bcdb6336
nuliows <nuliows@gmail.com>
2023-08-22 22:00:08 +0300

    some first code wars tasks

---
+++ src/main/java/lt/gerasimovas/credit_card_mask/CreditCardMask.java
@@ -0,0 +1,24 @@
+package lt.gerasimovas.credit_card_mask;
+
+public class CreditCardMask {
+    public static void main(String[] args) {
+        String firstTest = "4556364607935616";
+        String secondTest = "1";
+
+        System.out.println(maskify(secondTest));
+    }
+
+    public static String maskify(String str) {
+//        throw new UnsupportedOperationException("Unimplemented");
+        if (str.length() <= 4){
+            return str;
+        }
+        String result = "";
+        for (int i = 0; i < str.length()-4; i++) {
+            result += "#";
+        }
+        result += str.substring(str.length()-4);
+
+        return result;
+    }
+}
