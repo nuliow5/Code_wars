e90637438f391ccba8e26c6a1895767a8ffb3409
Vadimas Gerasimovas <vadimas@baltictransline.lt>
2023-10-23 17:17:36 +0300

    squareUp issues array = {-3, 2, 5} is work, but broken second test with array2 = {-3, -2, -1, 0, 3, 5}

--- src/main/java/lt/gerasimovas/simple_tasks/NumbersSquared.java
+++ src/main/java/lt/gerasimovas/simple_tasks/NumbersSquared.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class NumbersSquared {
     public static void main(String[] args) {
         int[] array = {-3, 2, 5};
+        int[] array2 = {-3, -2, -1, 0, 3, 5};
 
-        System.out.println(Arrays.toString(squareUp(array)));
+//        System.out.println(Arrays.toString(squareUp(array)));
+        System.out.println(Arrays.toString(squareUp(array2)));
     }
     //todo
     public static int[] squareUp(int[] numbers) {
@@ -15,13 +17,14 @@ public class NumbersSquared {
                 int modul = numbers[i] * -1;
 
                 for (int j = i + 1; j < numbers.length; j++) {
-                    if (modul >= numbers[j]) {
+                    if (modul <= numbers[j]) {
                         int newSpot = j-1;
 
                         for (int k = i; k < newSpot; k++) {
-                           numbers[k] = numbers[k+1];
+                           numbers[k] = numbers[k+1] * numbers[k+1];
                         }
                         numbers[newSpot] = modul * modul;
+                        i++;
                     }
                 }
 
